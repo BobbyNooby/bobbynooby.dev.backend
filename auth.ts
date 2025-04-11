c486604a653053cc4c2f78ed48115643deb4a7eb
BobbyNooby <theengineering.commandgamer@gmail.com>
2025-04-11 15:35:59 +0100

    Hopefully final auth changes

--- auth.ts
+++ auth.ts
@@ -21,36 +21,39 @@ if (IS_PRODUCTION == undefined) {
 }
 
 export const authConfig: ExpressAuthConfig = {
-	providers: [
-		Discord({
-			clientId: DISCORD_CLIENT_ID,
-			clientSecret: DISCORD_CLIENT_SECRET,
-			authorization: 'https://discord.com/api/oauth2/authorize?scope=identify'
-		})
-	],
-	trustHost: true,
-	callbacks: {
-		async jwt({ token, account, profile }) {
-			if (profile) {
-				token.id = profile.id;
-			}
-			return token;
-		},
-		async session({ session, token, user }) {
-			session.user.id = token.id as string;
-			return session;
-		}
-	},
-	cookies: {
-		sessionToken: {
-			name: 'sessionToken',
-			options: {
-				httpOnly: true,
-				sameSite: IS_PRODUCTION ? 'none' : 'lax',
-				secure: IS_PRODUCTION == 'true' ? true : false,
-				path: '/',
-				domain: IS_PRODUCTION ? '.bobbynooby.dev' : undefined
-			}
-		}
-	}
+  providers: [
+    Discord({
+      clientId: DISCORD_CLIENT_ID,
+      clientSecret: DISCORD_CLIENT_SECRET,
+      authorization: "https://discord.com/api/oauth2/authorize?scope=identify",
+    }),
+  ],
+  trustHost: true,
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (profile) {
+        token.id = profile.id;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+  cookies:
+    IS_PRODUCTION == "true"
+      ? {
+          sessionToken: {
+            name: "sessionToken",
+            options: {
+              httpOnly: true,
+              sameSite: "none",
+              secure: true,
+              path: "/",
+              domain: ".bobbynooby.dev",
+            },
+          },
+        }
+      : undefined,
 };
